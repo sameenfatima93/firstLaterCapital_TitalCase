49cfa3a793dcac1898a0b876c7f93ec693a980b1
SameenFatima <sameen.fatima993@gmail.com>
2025-09-16 10:11:18 +0500

    titalCase_threeMthodeDone

--- app.js
+++ app.js
@@ -38,5 +38,47 @@
 // console.log(finalResult);
 
 //______mathod__two___________
-//  var userSentence = prompt("enter any sentence")
-//  userSentence.split(" ");
+
+
+// var sentence = prompt("Enter a sentence:")// hello world
+// var words = sentence.split(" ") // ["hello", "world"]
+// // console.log(words);
+// var titleCaseWords = []
+
+// for (var i = 0; i < words.length; i++) {
+
+//     var firstChar = words[i].slice(0, 1) //h
+//     var otherChar = words[i].slice(1) //ello
+//     firstChar = firstChar.toUpperCase()  //H
+//     otherChar = otherChar.toLowerCase() //ello
+//     titleCaseWords.push(firstChar + otherChar)
+
+// }
+// console.log(titleCaseWords);
+
+
+
+
+////_______mathode___three____________//
+
+var fullName = prompt("enter your full name")
+var result = "";
+var temp = "";
+for (var i = 0; i < fullName.length; i++) {
+    if (i === 0) {
+        result += fullName[i].toUpperCase();
+    } else if (fullName[i] === " ") {
+        temp = fullName[i + 1].toUpperCase();
+        result += fullName[i];
+    }
+    else {
+        if (temp) {
+            result += temp;
+            temp = "";
+        } else {
+
+            result += fullName[i];
+        }
+    }
+}
+console.log(result);
