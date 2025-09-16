c1aefb5b26dc6867ab3fcdacbca5b4740ba14e78
SameenFatima <sameen.fatima993@gmail.com>
2025-09-16 09:53:27 +0500

    titalCaseTask

---
+++ app.js
@@ -0,0 +1,42 @@
+//___task_________1st latter capital_______________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////__________task_____create the tital case__________________//
+
+// ______mathod__one____________//
+
+// var userInput = prompt("Enter Any Name or Sentance");
+// var finalResult = "";
+// var flagMathod = true
+// for (var i = 0; i < userInput.length; i++) {
+//     if(flagMathod){
+//         finalResult += userInput[i].toUpperCase();
+//         flagMathod = false
+//     } else{
+//         finalResult += userInput[i].toLowerCase();
+//     }
+//     if(userInput[i] === " "){
+//         flagMathod = true
+
+//     }
+    
+// }
+// console.log(finalResult);
+
+//______mathod__two___________
+//  var userSentence = prompt("enter any sentence")
+//  userSentence.split(" ");
